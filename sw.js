f8d2033f7e3408a3fece939b937254d8b33142a6
contactmutale-lang <contactmutale@gmail.com>
2026-01-20 01:56:09 -0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'blackink-v3';
+const urlsToCache = [
+    './',
+    './index.html',
+    './style.css',
+    './tattoo-app.js',
+    './manifest.json',
+    './logo.jpg'
+];
+
+// Install the service worker and cache files
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            console.log('Opened cache');
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+// Serve files from cache when offline
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+// Clean up old caches
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
